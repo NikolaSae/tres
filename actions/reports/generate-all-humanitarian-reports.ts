eb1e33d68c72ffc6217b1ee2a20fb13ff2922b6b
NikolaSae <39310499+NikolaSae@users.noreply.github.com>
2026-02-09 12:53:18 +0000

    kiselavoda66

--- actions/reports/generate-all-humanitarian-reports.ts
+++ actions/reports/generate-all-humanitarian-reports.ts
@@ -1,7 +1,8 @@
 // /actions/reports/generate-all-humanitarian-reports.ts
 'use server';
 
-import { generateAllHumanitarianReports as innerGenerate, TemplateType, PaymentType } from './humanitarian/index';
+import { generateAllHumanitarianReports as innerGenerate } from './humanitarian/index';
+import { TemplateType, PaymentType } from './humanitarian/types';
 
 export async function generateAllHumanitarianReports(
   month: number,
