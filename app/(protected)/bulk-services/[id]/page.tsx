1d25f04dc2476a3afe411317301442e56ca61df3
NikolaSae <39310499+NikolaSae@users.noreply.github.com>
2026-02-10 13:29:31 +0000

    malimrav01234

--- app/(protected)/bulk-services/[id]/page.tsx
+++ app/(protected)/bulk-services/[id]/page.tsx
@@ -55,7 +55,7 @@ export default async function BulkServiceDetailsPage({
       <Separator />
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
         <BulkServiceDetails bulkService={bulkService} />
-        <BulkServiceStats bulkService={bulkService} />
+        <BulkServiceStats bulkServices={[bulkService]} />
       </div>
     </div>
   );
